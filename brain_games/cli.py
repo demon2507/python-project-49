a2b96738f18e6be1bc9e09239193163d825945d7
dima Minakov <vano25071993@yandex.ru>
2025-01-06 20:45:32 +0700

    fix erros of linter ruff

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,10 +1,11 @@
 import prompt
 
+
 def welcome_user():
     name = ''
-    while name == '':
-      print('May I have your name? ', end='')
-      name = input()
+    while name == '': 
+        print('May I have your name? ', end='') 
+        name = prompt.string('May I have your name? ')
     print('Hello, ' + name + '!')
 
 
