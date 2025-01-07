f047d0dab2048ab5fd6ed3cfe9c3582a9311c84b
dima Minakov <vano25071993@yandex.ru>
2025-01-07 16:27:48 +0700

    making game is_even

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,31 @@
+import prompt
+from random import randint
+
+print("Welcome to the Brain Games!")
+name = prompt.string("May I have your name? ")
+print(f'Hello, {name}!')
+print('Answer \"yes\" if the number is even, otherwise answer \"no\"')
+
+def is_even(name):
+    qwestion = 'Qwestion:'
+    for _ in range(0, 3):
+        number = randint(1, 100)
+        is_number_even = 'no' if number % 2 != 0 else 'yes';
+        print(f'{qwestion} {number}')
+        answer = prompt.string('Your answer: ')
+        if(answer == is_number_even):
+            print('Correct!')
+        else: 
+            print(f'{answer} is wrong answer ;(. Correct answer was {is_number_even}.\nLet\'s try again, {name}!')
+            return
+
+    print(f'Congratulations, {name}!')
+
+
+is_even(name)
+        
+
+
+        
+
+    
